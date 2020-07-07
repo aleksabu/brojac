a26cdd6b8ed92ac6bf1683eaedb977c913b004e8
Aleksa <aleksabudrak@gmail.com>
2020-07-08 00:56:02 +0200

    dodavanje app index styles

---
+++ app.js
@@ -0,0 +1,16 @@
+let count = 0;
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }else if(styles.contains("increase")){
+            count++;
+        }if(styles.contains("reset")){
+            count = 0;
+        }value.textContent = count;
+    });
+})
